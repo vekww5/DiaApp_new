68bbc13cac623937efc1ee70aafeafa069bf1e7b
vekww5 <vekww5@gmail.com>
2023-08-31 12:35:45 +0300

    new

--- app/src/main/java/com/example/diaapp/database/SingData.java
+++ app/src/main/java/com/example/diaapp/database/SingData.java
@@ -6,14 +6,18 @@ import androidx.room.PrimaryKey;
     @Entity(tableName = "SingData")public class SingData {
         @PrimaryKey(autoGenerate = true)
         public int uid;
-        @ColumnInfo(name = "username")    String username;
+        @ColumnInfo(name = "username")
+        String username;
         @ColumnInfo(name = "email")
         String email;
-        @ColumnInfo(name = "password")    String password;
+        @ColumnInfo(name = "password")
+        String password;
         @ColumnInfo(name = "repeatPassword")
         String repeatPassword;
-        void validate() {        //if (email.isBlank()) throw EmptyFieldException(Field.Email);
-            //if (username.isBlank()) throw EmptyFieldException(Field.Username);        //if (password.isBlank()) throw EmptyFieldException(Field.Password);
+        void validate() {
+            //if (email.isBlank()) throw EmptyFieldException(Field.Email);
+            //if (username.isBlank()) throw EmptyFieldException(Field.Username);
+            //if (password.isBlank()) throw EmptyFieldException(Field.Password);
             //if (password != repeatPassword) throw PasswordMismatchException();    }
         }
 }
